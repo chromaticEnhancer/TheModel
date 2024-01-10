33b6d75bef0b3f54da721be90b70db59bfb7815f
abhinas regmi <aviregmi2018@gmail.com>
2024-01-10 18:26:32 +0545

    Migrated to setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='themodel',
+    version='1.0.0',
+    description='A python package that takes your boring b&W manga and colorises it.',
+    python_requires='>=3.10, <3.12',
+    install_requires=[
+        'torch==2.1.2+cu121',
+        'torchvision==0.16.2+cu121',
+        'matplotlib',
+        'pydantic',
+        'pydantic_settings',
+    ]
+)
