22cdc755b1a42bad57deadcc932cd320a4985725
abhinas regmi <aviregmi2018@gmail.com>
2024-01-10 22:00:08 +0545

    Add white color penalty loss function and import necessary modules

---
+++ src/themodel/losses.py
@@ -0,0 +1,9 @@
+import torch
+
+
+def white_color_penalty_loss(actual_image: torch.Tensor, generated_image: torch.Tensor):
+    mask = (~((actual_image > 0.85).float().sum(dim = 1) == 3).unsqueeze(1).repeat((1, 3, 1, 1 ))).float()
+    white_zones = mask * (generated_image + 1) / 2
+    white_penalty = (torch.pow(white_zones.sum(dim = 1), 2).sum(dim = (1, 2)) / (mask.sum(dim = (1, 2, 3)) + 1)).mean()
+
+    return white_penalty
---
+++ src/themodel/train.py
@@ -0,0 +1,5 @@
+from themodel.generator import UNet
+from themodel.discriminator import PatchGAN
+
+from themodel.config import settings
+from themodel.utils import save_model, load_model
