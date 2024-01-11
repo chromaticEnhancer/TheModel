9af584cf80ef238bc81aef7e0caab075bc4345a3
abhinas regmi <aviregmi2018@gmail.com>
2024-01-11 12:48:50 +0545

    Update torch and torchvision dependencies

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description='A python package that takes your boring b&W manga and colorises it.',
     python_requires='>=3.10, <3.12',
     install_requires=[
-        'torch==2.1.2+cu121',
-        'torchvision==0.16.2+cu121',
+        'torch',
+        'torchvision',
         'matplotlib',
         'pydantic',
         'pydantic_settings',
