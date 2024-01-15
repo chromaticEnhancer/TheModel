862539e99c2b53b308a7138f492aa5d5b38430c6
abhinas regmi <aviregmi2018@gmail.com>
2024-01-15 13:24:10 +0545

    Update version number and fix colorization bug

--- src/themodel/interface.py
+++ src/themodel/interface.py
@@ -9,13 +9,14 @@ settings.IMAGE_HEIGHT = 184
 settings.IMAGE_WIDTH = 128
 
 def colorize(input_path: str, output_path: str):
-    model = UNet(in_channels=1, out_channels=3)
+    model = UNet(in_channels=3, out_channels=3)
     load_generator(model=model, checkpoint_type=CheckpointTypes.COLOR_GENERATOR)
 
     image = torchvision.io.read_image(path=input_path, mode=torchvision.io.ImageReadMode.GRAY)
     normalize = normalize_image(is_color=False)
     
     image = normalize(image / 255.0)
+    image = image.repeat(3, 1, 1)
     out = model(image.unsqueeze(0))
     out = out * 255
     out = out.squeeze(0).byte()
