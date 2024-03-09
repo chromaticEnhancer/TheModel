d3b31a2151306b045fce49a511c9a3eac8e449d7
Abi Shrestha <60970080+scifisatan@users.noreply.github.com>
2024-03-09 23:48:05 +0545

    remove print_options

--- src/themodel/options/base_options.py
+++ src/themodel/options/base_options.py
@@ -70,24 +70,7 @@ class BaseOptions():
         return parser.parse_args()
 
     def print_options(self, opt):
-        message = ''
-        message += '----------------- Options ---------------\n'
-        for k, v in sorted(vars(opt).items()):
-            comment = ''
-            default = self.parser.get_default(k)
-            if v != default:
-                comment = '\t[default: %s]' % str(default)
-            message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
-        message += '----------------- End -------------------'
-        print(message)
-
-        # save to the disk
-        expr_dir = os.path.join(opt.checkpoints_dir, opt.name)
-        util.mkdirs(expr_dir)
-        file_name = os.path.join(expr_dir, 'opt.txt')
-        with open(file_name, 'wt') as opt_file:
-            opt_file.write(message)
-            opt_file.write('\n')
+        pass
 
     def parse(self):
 
